9c06f1008447015f17fb586797b794887d8ac311
ZDUYTHIENZ\Admins <zduythienz@gmail.com>
2020-01-02 22:17:56 +0700

    change transitionConfig: right to left

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -4,13 +4,69 @@ import { createStackNavigator } from 'react-navigation-stack';
 import { HomeScreen } from '../pages/Home';
 import { DetailsScreen } from '../pages/Item';
 
+// const HomeNavigator = createStackNavigator(
+//   {
+//     Home: HomeScreen,
+//     Details: DetailsScreen,
+//   },
+//   {
+//     headerMode: 'none',
+//     transitionConfig: () => ({
+//       screenInterpolator: SlideFromRightIOS,
+//     }),
+//     gesturesEnabled: true,
+//   },
+// );
+
 const HomeNavigator = createStackNavigator(
+  { Home: HomeScreen, Details: DetailsScreen },
   {
-    Home: HomeScreen,
-    Details: DetailsScreen,
-  },
-  {
-    headerMode: 'none',
+    mode: 'card',
+    navigationOptions: params => ({
+      gesturesEnabled: true,
+      gesturesDirection: 'inverted',
+    }),
+    transitionConfig: () => ({
+      screenInterpolator: sceneProps => {
+        const { layout, position, scene } = sceneProps;
+        const { index } = scene;
+        const width = layout.initWidth;
+
+        return {
+          opacity: position.interpolate({
+            inputRange: [index - 1, index, index + 1],
+            outputRange: [0, 1, 0],
+          }),
+          transform: [
+            {
+              translateX: position.interpolate({
+                inputRange: [index - 1, index, index + 1],
+                outputRange: [-width, 0, width],
+              }),
+            },
+          ],
+        };
+      },
+      // headerTitleInterpolator: sceneProps => {
+      //   const { layout, position, scene } = sceneProps;
+      //   const { index } = scene;
+
+      //   return {
+      //     opacity: position.interpolate({
+      //       inputRange: [index - 1, index, index + 1],
+      //       outputRange: [0, 1, 0],
+      //     }),
+      //     transform: [
+      //       {
+      //         translateX: position.interpolate({
+      //           inputRange: [index - 1, index, index + 1],
+      //           outputRange: [-50, 0, 50],
+      //         }),
+      //       },
+      //     ],
+      //   };
+      // },
+    }),
   },
 );
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import {SafeAreaView} from 'react-navigation';
-import {Button, Divider, Layout, TopNavigation} from '@ui-kitten/components';
+import { SafeAreaView } from 'react-navigation';
+import { Button, Divider, Layout, TopNavigation } from '@ui-kitten/components';
 
-export const HomeScreen = ({navigation}) => {
+export const HomeScreen = ({ navigation }) => {
   const navigateDetails = () => {
-    navigation.navigate('Details');
+    navigation.navigate({ routeName: 'Details', transitionStyle: 'inverted' });
   };
 
   return (
-    <SafeAreaView style={{flex: 1}}>
+    <SafeAreaView style={{ flex: 1 }}>
       <TopNavigation title="MyApp" alignment="center" />
       <Divider />
-      <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Button onPress={navigateDetails}>OPEN DETAILS</Button>
       </Layout>
     </SafeAreaView>
